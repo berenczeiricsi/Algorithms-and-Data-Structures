4ae95c56cf7b61ba13107211538c8bebc7299bd0
berenczeiricsi <berenczeijr@gmail.com>
2024-09-12 11:39:53 +0200

    Initializing the Binary Search Tree.

---
+++ Binary-Search-Tree/bst.py
@@ -0,0 +1,163 @@
+from typing import Any, Generator, Tuple
+
+from tree_node import TreeNode
+
+
+class BinarySearchTree:
+    """Binary-Search-Tree implemented for didactic reasons."""
+
+    def __init__(self, root: TreeNode = None):
+        """Initialize BinarySearchTree.
+
+        Args:
+            root (TreeNode, optional): Root of the BST. Defaults to None.
+        
+        Raises:
+            ValueError: root is neither a TreeNode nor None.
+        """
+        self._root = root
+        self._size = 0 if root is None else 1
+        self._num_of_comparisons = 0
+
+    def insert(self, key: int, value: Any) -> None:
+        """Insert a new node into BST.
+
+        Args:
+            key (int): Key which is used for placing the value into the tree.
+            value (Any): Value to insert.
+
+        Raises:
+            ValueError: If key is not an integer.
+            KeyError: If key is already present in the tree.
+        """
+        pass
+        # TODO
+
+    def find(self, key: int) -> TreeNode:
+        """Return node with given key.
+
+        Args:
+            key (int): Key of node.
+
+        Raises:
+            ValueError: If key is not an integer.
+            KeyError: If key is not present in the tree.
+
+        Returns:
+            TreeNode: Node
+        """
+        pass
+        # TODO
+
+    @property
+    def size(self) -> int:
+        """Return number of nodes contained in the tree."""
+        pass
+        # TODO
+
+    # If users instead call `len(tree)`, this makes it return the same as `tree.size`
+    __len__ = size 
+
+    # This is what gets called when you call e.g. `tree[5]`
+    def __getitem__(self, key: int) -> Any:
+        """Return value of node with given key.
+
+        Args:
+            key (int): Key to look for.
+
+        Raises:
+            ValueError: If key is not an integer.
+            KeyError: If key is not present in the tree.
+
+        Returns:
+            Any: [description]
+        """
+        return self.find(key).value
+
+    def remove(self, key: int) -> None:
+        """Remove node with given key, maintaining BST-properties.
+
+        Args:
+            key (int): Key of node which should be deleted.
+
+        Raises:
+            ValueError: If key is not an integer.
+            KeyError: If key is not present in the tree.
+        """
+        pass
+        # TODO
+       
+    # Hint: The following 3 methods can be implemented recursively, and 
+    # the keyword `yield from` might be extremely useful here:
+    # http://simeonvisser.com/posts/python-3-using-yield-from-in-generators-part-1.html
+
+    # Also, we use a small syntactic sugar here: 
+    # https://www.pythoninformer.com/python-language/intermediate-python/short-circuit-evaluation/
+
+    def inorder(self, node: TreeNode = None) -> Generator[TreeNode, None, None]:
+        """Yield nodes in inorder."""
+        node = node or self._root
+        # This is needed in the case that there are no nodes.
+        if not node:
+            return iter(())
+        yield from self._inorder(node)
+
+    def preorder(self, node: TreeNode = None) -> Generator[TreeNode, None, None]:
+        """Yield nodes in preorder."""
+        node = node or self._root
+        if not node:
+            return iter(())
+        yield from self._preorder(node)
+
+    def postorder(self, node: TreeNode = None) -> Generator[TreeNode, None, None]:
+        """Yield nodes in postorder."""
+        node = node or self._root
+        if not node:
+            return iter(())
+        yield from self._postorder(node)
+
+    # this allows for e.g. `for node in tree`, or `list(tree)`.
+    def __iter__(self) -> Generator[TreeNode, None, None]: 
+        yield from self._preorder(self._root)
+
+    @property
+    def is_valid(self) -> bool:
+        """Return if the tree fulfills BST-criteria."""
+        pass
+        # TODO
+    def return_max_key(self) -> TreeNode:
+            """Return the node with the largest key (None if tree is empty)."""
+            pass
+
+    def find_comparison(self, key: int) -> Tuple[int, int]:
+        """Create an inbuilt python list of BST values in preorder and compute the number of comparisons needed for
+           finding the key both in the list and in the BST.
+           Return the numbers of comparisons for both, the list and the BST
+        """
+        python_list = list(node.key for node in self._preorder())
+        # TODO
+
+    def __repr__(self) -> str:
+        return f"BinarySearchTree({list(self._inorder(self._root))})"
+
+    ####################################################
+    # Helper Functions
+    ####################################################
+
+    def get_root(self):
+        return self._root
+
+    def _inorder(self, current_node):
+        pass
+        # TODO
+
+    def _preorder(self, current_node):
+        pass
+        # TODO
+
+    def _postorder(self, current_node):
+        pass
+        # TODO
+
+    # You can of course add your own methods and/or functions!
+    # (A method is within a class, a function outside of it.)
---
+++ Binary-Search-Tree/tree_node.py
@@ -0,0 +1,50 @@
+from typing import Any
+
+
+class TreeNode:
+    
+    def __init__(self, key: int, value: Any, right: 'TreeNode' = None, 
+                 left: 'TreeNode' = None, parent: 'TreeNode' = None):
+        """Initialize TreeNode.
+
+        Args:
+            key (int): Key used for sorting the node into a BST.
+            value (Any): Whatever data the node shall carry.
+            right (TreeNode, optional): Node to the right, with a larger key. Defaults to None.
+            left (TreeNode, optional): Node to the left, with a lesser key. Defaults to None.
+            parent (TreeNode, optional): Parent node. Defaults to None.
+        """
+        self.key = key
+        self.value = value
+        self.right = right
+        self.left = left
+        self.parent = parent
+
+    def __repr__(self) -> str:
+        return f"TreeNode({self.key}, {self.value})"
+
+    @property
+    def depth(self) -> int:
+        """Return depth of the node, i.e. the number of parents/grandparents etc.
+
+        Returns:
+            int: Depth of node
+        """
+        pass
+        # TODO
+
+    @property
+    def is_external(self) -> bool:
+        """Return if node is an external node (= leaf)."""
+        pass
+        # TODO
+   
+    @property
+    def is_internal(self) -> bool: 
+        """Return if node is an internal node."""
+        pass
+        # TODO
+ 
+    # You can of course add your own methods and/or functions!
+    # (A method is within a class, a function outside of it.)
+
